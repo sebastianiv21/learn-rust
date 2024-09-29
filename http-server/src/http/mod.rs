825267894c5ef10f39b69113bc073aa865129774
sebastianiv21 <seluti98@gmail.com>
2024-09-28 19:33:19 -0500

    session 10

--- http-server/src/http/mod.rs
+++ http-server/src/http/mod.rs
@@ -1,7 +1,11 @@
 mod request;
 mod method;
 mod query_string;
+mod response;
+mod status_code;
 
 pub use request::{Request, ParseError};
+pub use response::Response;
+pub use status_code::StatusCode;
 pub use method::Method;
 pub use query_string::{QueryString, Value as QueryStringValue};
